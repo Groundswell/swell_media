90a67b6283992a1f293f0e9730021b4ea33a451b
Michael <meister@spacekace.com>
2017-12-04 11:21:58 -0800

    invalid is a reserved word

--- app/models/swell_media/email.rb
+++ app/models/swell_media/email.rb
@@ -3,7 +3,7 @@ module SwellMedia
 	class Email < ActiveRecord::Base
 		self.table_name = 'emails'
 
-		enum status: { 'invalid' => -1, 'pending' => 0, 'confirmed' => 1 }
+		enum status: { 'failed' => -1, 'pending' => 0, 'confirmed' => 1 }
 
 		validates_format_of :address, with: Devise.email_regexp, if: :address_changed?
 
