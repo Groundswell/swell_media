f6f1a6d0a0b7a781845814630c187eb65f999575
Michael <meister@spacekace.com>
2017-12-04 11:19:42 -0800

    email model

---
+++ app/models/swell_media/email.rb
@@ -0,0 +1,11 @@
+module SwellMedia
+
+	class Email < ActiveRecord::Base
+		self.table_name = 'emails'
+
+		enum status: { 'invalid' => -1, 'pending' => 0, 'confirmed' => 1 }
+
+		validates_format_of :address, with: Devise.email_regexp, if: :address_changed?
+
+	end
+end
