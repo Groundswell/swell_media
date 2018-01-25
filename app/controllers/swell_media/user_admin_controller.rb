8b8f7bfa4dc907dc1824a5e29264415b180b462f
Gk Parish-Philp <gk@gkparishphilp.com>
2018-01-25 13:13:29 -0800

    take nick out of eng, its an app thing

--- app/controllers/swell_media/user_admin_controller.rb
+++ app/controllers/swell_media/user_admin_controller.rb
@@ -6,9 +6,7 @@ module SwellMedia
 		def create
 			email = params[:user][:email]
 
-			nick = params[:user][:name].split( /\s+/ ).first unless params[:user][:name].blank?
-
-			user_attributes = { email: email, full_name: params[:user][:name], nickname: nick, name: params[:user][:name].parameterize, ip: request.ip }
+			user_attributes = { email: email, full_name: params[:user][:name], name: params[:user][:name].parameterize, ip: request.ip }
 
 			if User.where( email: email ).first.present?
 				# this email is already registered for this site
