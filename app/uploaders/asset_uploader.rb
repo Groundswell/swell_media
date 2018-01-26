697cdcd23d1fc289041c502bed991639696c5092
Michael <meister@spacekace.com>
2018-01-26 13:25:55 -0800

    small, probably unnecessary adjustments

--- app/uploaders/asset_uploader.rb
+++ app/uploaders/asset_uploader.rb
@@ -3,13 +3,13 @@
 if defined?(CarrierWave)
 
 	class AssetUploader < CarrierWave::Uploader::Base
-		include CarrierWaveDirect::Uploader if defined?(CarrierWaveDirect)
-
 		storage :fog
+		# include CarrierWaveDirect::Uploader if defined?(CarrierWaveDirect)
 
-		def asset_host
-			SwellMedia.asset_host || super
-		end
+
+		# def asset_host
+		# 	SwellMedia.asset_host || super
+		# end
 
 		# Override the directory where uploaded files will be stored.
 		# This is a sensible default for uploaders that are meant to be mounted:
