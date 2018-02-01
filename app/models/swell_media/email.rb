6f44a9ecf1404174a2195c5a8ed4e8fc6465b280
Michael <meister@spacekace.com>
2018-02-01 08:41:48 -0800

    add user to email and contacts
    so we can track them to a user, if one exists, or comes into existance

--- app/models/swell_media/email.rb
+++ app/models/swell_media/email.rb
@@ -5,6 +5,8 @@ module SwellMedia
 
 		enum status: { 'failed' => -1, 'pending' => 0, 'confirmed' => 1 }
 
+		belongs_to	:user, class_name: SwellMedia.registered_user_class, optional: true
+
 		validates_format_of :address, with: Devise.email_regexp, if: :address_changed?
 
 	end
