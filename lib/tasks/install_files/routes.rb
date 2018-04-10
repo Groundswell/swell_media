01dbc5234636572f22eba70c2e152a6ec005c592
Michael <meister@spacekace.com>
2018-04-10 11:25:09 -0700

    update routes install file

--- lib/tasks/install_files/routes.rb
+++ lib/tasks/install_files/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
 	devise_scope :user do
+		get '/forgot' => 'passwords#new', as: 'forgot'
 		get '/login' => 'sessions#new', as: 'login'
 		get '/logout' => 'sessions#destroy', as: 'logout'
 		get '/register' => 'registrations#new', as: 'register'
 	end
-	devise_for :users, :controllers => { :omniauth_callbacks => 'oauth', :registrations => 'registrations', :sessions => 'sessions' }
+	devise_for :users, :controllers => { :omniauth_callbacks => 'oauth', :registrations => 'registrations', :sessions => 'sessions', :passwords => 'passwords' }
 
 
 	mount SwellMedia::Engine, :at => '/'
