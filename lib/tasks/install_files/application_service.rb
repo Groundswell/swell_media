c6a8b2a6148e302a4b9dbb2c99ea09eec59b91f4
Michael Ferguson <meister@spacekace.com>
2018-05-15 14:51:49 -0700

    wrap in rescue block

--- lib/tasks/install_files/application_service.rb
+++ lib/tasks/install_files/application_service.rb
@@ -3,8 +3,13 @@ class ApplicationService
 
 	def log_event( options={} )
 		if defined?( Bunyan )
-			@event_service ||= Bunyan::EventService.new
-			@event_service.log_event( options )
+			begin
+				@event_service ||= Bunyan::EventService.new
+				@event_service.log_event( options )
+			rescue Exception => e
+				raise e unless Rails.env.production?
+				NewRelic::Agent.notice_error(e) if defined?( NewRelic )
+			end
 		end
 	end
 
